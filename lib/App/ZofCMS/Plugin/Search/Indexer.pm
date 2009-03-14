5b1bc70472704670198c7da9720bfbb6981f08a7
Zoffix Znet <zoffixcpan@gmail.com>
2009-03-14 07:38:19 +0000

    Import of ZOFFIX/App-ZofCMS-Plugin-Search-Indexer-0.0102 from CPAN.
    gitpan-cpan-distribution: App-ZofCMS-Plugin-Search-Indexer
    gitpan-cpan-version:      0.0102
    gitpan-cpan-path:         ZOFFIX/App-ZofCMS-Plugin-Search-Indexer-0.0102.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

--- lib/App/ZofCMS/Plugin/Search/Indexer.pm
+++ lib/App/ZofCMS/Plugin/Search/Indexer.pm
@@ -3,7 +3,7 @@ package App::ZofCMS::Plugin::Search::Indexer;
 use warnings;
 use strict;
 
-our $VERSION = '0.0101';
+our $VERSION = '0.0102';
 
 use base 'App::ZofCMS::Plugin::Base';
 use Search::Indexer;
